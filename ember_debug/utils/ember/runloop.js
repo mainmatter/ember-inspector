239f1bd5c5daac33c0ffd9a83c4012ddc39e077a
Marine Dunstetter <bluecut.official@gmail.com>
2025-06-24 15:43:08 +0200

    TMP (test): if we remove usage of the own-runloop

--- ember_debug/utils/ember/runloop.js
+++ ember_debug/utils/ember/runloop.js
@@ -1,12 +1,14 @@
 import Ember, { Runloop as EmberRunloop } from '../ember';
-import * as runloop from './own-runloop';
+// import * as ownRunloop from './own-runloop';
 
 // it could happen that runloop is available but _backburner is not exported (dead code)
 // then we need to use our own.
-let module = runloop;
-let _backburner = runloop._backburner;
+// let module = ownRunloop;
+// let _backburner = ownRunloop._backburner;
+let module;
+let _backburner;
 
-const keys = ['cancel', 'debounce', 'join', 'later', 'scheduleOnce'];
+// const keys = ['cancel', 'debounce', 'join', 'later', 'scheduleOnce'];
 
 if (EmberRunloop) {
   module = EmberRunloop;
@@ -18,20 +20,21 @@ if (EmberRunloop) {
   _backburner = Ember?.run?.backburner || _backburner;
 }
 
-if (!keys.every((k) => k in module)) {
-  module = runloop;
-}
+// if (!keys.every((k) => k in module)) {
+//   module = ownRunloop;
+// }
 
 // if it is our own, run a internal to trigger `end`
 // required in object inspector & render debug
-function loop() {
-  _backburner.later('actions', loop, 300);
-}
+// function loop() {
+//   _backburner.later('actions', loop, 300);
+// }
 
-if (_backburner === runloop._backburner) {
-  loop();
-}
+// if (_backburner === ownRunloop._backburner) {
+//   loop();
+// }
 
-export let run = runloop.run;
+// export let run = ownRunloop.run;
 export { _backburner };
-export let { cancel, debounce, join, later, scheduleOnce } = module;
+// export let { cancel, debounce, join, later, scheduleOnce } = module;
+export let { cancel, debounce, join, later, run, scheduleOnce } = module;
